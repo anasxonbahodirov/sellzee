4f9af3ba8bc83f480af67c1cb67d0ba7dd5fd2b6
anasxonbahodirov <anasxonbahodirov1@gmail.com>
2024-10-17 10:29:40 +0500

    qoshdim file

---
+++ src/components/container.jsx
@@ -0,0 +1,9 @@
+export { Container } from "./container";
+
+export const Container = ({children}) => {
+    return (
+      <div className="container mx-auto px-4 max-w-[1134px]">
+          {children}
+      </div>
+    )
+  }
